e851a938fb180ff076a3a527103957c59d13799a
JimScope <jimscope@protonmail.com>
2022-08-12 09:33:28 -0400

    Added string metrics analyser

--- client.py
+++ client.py
@@ -1,29 +1,68 @@
+import argparse
 import configparser
 import logging
+import random
 import socket
+import string
 
 config = configparser.ConfigParser()
-config.read('config.ini')
-config_def = config['DEFAULT']
-logging.basicConfig(level=logging.DEBUG)
+config.read("config.ini")
+config_def = config["DEFAULT"]
+logging.basicConfig(
+    level=logging.DEBUG, format="%(asctime)s - %(message)s", filename="client.log"
+)
 
 
-def main():
+def main() -> None:
+    parser = argparse.ArgumentParser(description="String generator")
+    parser.add_argument(
+        "-c",
+        "--count",
+        help="Count of strings to generate",
+        required=False,
+        default=1000000,
+        type=int,
+    )
+
+    args = parser.parse_args()
+    generate_file(args)
+
+
+def generate_file(args: argparse.Namespace) -> None:
+    """Generate a file with random strings
+    :param args: argparse.Namespace
+    :return: None
+    """
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect((config_def['ADDRESS'], int(config_def['PORT'])))
+    client.connect((config_def["ADDRESS"], int(config_def["PORT"])))
     logging.info(f"Connection established with the server")
 
-    with open(f"{config_def['FILE_PATH']}", "r") as f:
-        send_data = f.read()
+    with open(config_def["FILE_PATH"], "w") as file:
+        for _ in range(int(args.count)):
+            len = random.randint(50, 100)
+            str = random.choices(
+                string.ascii_uppercase + string.ascii_lowercase + string.digits, k=len
+            )
+            spaces_count = random.randint(3, 5)
+            spaces = set()
 
-    client.send(send_data.encode(config_def['FORMAT']))
-    logging.info("File sended")
+            for _ in range(spaces_count):
+                s = random.choice([x for x in range(1, len) if x not in spaces])
+                spaces.add(s)
+                spaces.add(s - 1)
+                spaces.add(s + 1)
+                str[s] = " "
 
-    server_response = client.recv(int(config_def['SIZE'])).decode(config_def['FORMAT'])
-    logging.info(f"OK: {server_response}")
-
-    logging.info("Disconnected from the server")
-    client.close()
+            file.write("".join(str) + "\n")
+            client.send("".join(str).encode(config_def["FORMAT"]))
+            logging.info("String sended")
+            server_response = client.recv(int(config_def["SIZE"])).decode(
+                config_def["FORMAT"]
+            )
+            logging.info(f"Response from server received: {server_response}")
+        client.send("END_SESSION".encode(config_def["FORMAT"]))
+        logging.info("Disconnected from the server")
+        client.close()
 
 
 if __name__ == "__main__":
--- server.py
+++ server.py
@@ -1,39 +1,73 @@
 import configparser
 import logging
 import socket
-import threading
+from concurrent.futures import ThreadPoolExecutor
+from time import perf_counter
 
 IP = socket.gethostbyname(socket.gethostname())
 config = configparser.ConfigParser()
 config.read("config.ini")
 config_def = config["DEFAULT"]
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(
+    level=logging.DEBUG, format="%(asctime)s - %(message)s", filename="server.log"
+)
 
 
-def handle_connections(connection, address) -> None:
+def handle_connections(connection: socket.socket, address: tuple) -> None:
+    """ Handle the connections
+    :param connection: socket.socket
+    :param address: tuple
+    :return: None"""
     logging.info(f"Connection established with: {address}")
+    start = perf_counter()
+    while True:
+        try:
+            data = connection.recv(int(config_def["SIZE"])).decode(config_def["FORMAT"])
 
-    data = connection.recv(int(config_def["SIZE"])).decode(config_def["FORMAT"])
-    logging.debug("Data received")
-    connection.send("OK".encode(config_def["FORMAT"]))
+            # Check for the signal to end the session
+            if data == "END_SESSION":
+                finish = perf_counter()
+                connection.send(
+                    f"Process completed in: {finish - start}".encode(
+                        config_def["FORMAT"]
+                    )
+                )
+                logging.info(f"{address} disconnected")
+                connection.close()
+                break
 
-    logging.info(f"{address} disconnected")
-    connection.close()
+            # Filter the data
+            elif "aa" in data.lower():
+                logging.info(f"Double 'a' rule detected from {address} >> {data}")
+                metrics = 1000
+            else:
+                logging.debug(f"Data received {data}")
+                spaces_count = data.count(" ")
+                letters_count = sum(1 for c in data if c.isalpha())
+                digits_count = sum(1 for c in data if c.isdigit())
+                # Metrics Calculation
+                metrics = (letters_count * 1.5 + digits_count * 2) / spaces_count
+
+            connection.send(f"Metrics: {metrics}".encode(config_def["FORMAT"]))
+
+        except ConnectionResetError:
+            logging.info("Connection closed")
+            break
 
 
 def main():
     logging.info("Server is starting")
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind((config_def["ADDRESS"], int(config_def["PORT"])))
+    server.bind((config_def.get("ADDRESS"), int(config_def.get("PORT"))))
     server.listen()
-    logging.info(f"Server is listening on {IP}:{config_def['PORT']}")
+    logging.info(f"Server is listening on {IP}:{config_def.get('PORT')}")
 
-    while True:
-        connection, address = server.accept()
-        thread = threading.Thread(target=handle_connections, args=(connection, address))
-        thread.start()
-        logging.info(f"Active Connections {threading.active_count() - 1}")
+    # Create a thread pool to handle the connections
+    with ThreadPoolExecutor(max_workers=int(config_def.get("MAX_WORKERS"))) as executor:
+        while True:
+            connection, address = server.accept()
+            executor.submit(handle_connections, connection, address)
 
 
 if __name__ == "__main__":
